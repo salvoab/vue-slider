413259fdf2fc5fdb03cb62e347ddaeac4a851704
salvoab <salvatore.abate85@gmail.com>
2020-12-15 16:18:13 +0100

    Scorrimento immagini a destra e sinistra con metodi

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,19 @@ let app = new Vue({
             'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
             'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
         ]
+    },
+    methods: {
+        nextImage(){
+            this.index++;
+            if(this.index >= this.srcImages.length){
+                this.index = 0;
+            }
+        },
+        prevImage(){
+            this.index--;
+            if(this.index<0){
+                this.index = this.srcImages.length - 1;
+            }
+        }
     }
 });

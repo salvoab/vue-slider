eb90d6c0c7aaa647e17c5384463642f3fd6da7e3
salvoab <salvatore.abate85@gmail.com>
2020-12-15 16:09:31 +0100

    Mostrata la prima immagine dello slider

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//Ricreare lo slider di immagini ma questa volta con Vue
+let app = new Vue({
+    el: '#root',
+    data: {
+        index: 0,
+        srcImages: [
+            'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+            'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+            'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+            'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+        ]
+    }
+});

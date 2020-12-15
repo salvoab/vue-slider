25e586575f99bb4ca3becb9a3898524dfb95f9f1
salvoab <salvatore.abate85@gmail.com>
2020-12-15 16:35:23 +0100

    Bonus: lo slider scorre in avanti ogni 3 secondi

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,33 @@
+//Ricreare lo slider di immagini ma questa volta con Vue
+/*  BONUS
+    Facciamo in modo che il nostro slider scorra automaticamente in avanti,
+    e ogni 3 secondi visualizzi un'immagine differente.
+*/
+let app = new Vue({
+    el: '#root',
+    data: {
+        index: 0,
+        srcImages: [
+            'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+            'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+            'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+            'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+        ]
+    },
+    methods: {
+        nextImage(){
+            this.index++;
+            if(this.index >= this.srcImages.length){
+                this.index = 0;
+            }
+        },
+        prevImage(){
+            this.index--;
+            if(this.index<0){
+                this.index = this.srcImages.length - 1;
+            }
+        }
+    }
+});
+
+setInterval(app.nextImage,3000);
